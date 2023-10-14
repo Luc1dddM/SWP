fa5aaac3b54de32a2e88661a60ff4b7994ab2d7a
Hoàng Vũ Thái <thaihvce170275@fpt.edu.vn>
2023-10-14 11:47:01 +0700

    Add view for service_Hoang Vu Thai

---
+++ SWP_CarService_Final/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWP_CarService_Final.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult ListOfServices()
+        {
+            return View();
+        }
+        public IActionResult AddService()
+        {
+            return View();
+        }
+        public IActionResult EditService()
+        {
+            return View();
+        }
+    }
+}
