0cede6fa4443d817f42eb6831720910c84cc92a4
Lam Nguyễn Sơn <lam6556@outlook.com>
2023-10-19 07:49:40 +0700

    commit create, view work order

--- SWP_CarService_Final/Areas/User/Controllers/OrderController.cs
+++ SWP_CarService_Final/Areas/User/Controllers/OrderController.cs
@@ -25,13 +25,29 @@ namespace Areas
             return View();
         }
 
+        public IActionResult view()
+        {
+            string cUserString = _contx.HttpContext.Session.GetString("cUser");
+            List<WorkOrder> orders;
+            User cUser = JsonConvert.DeserializeObject<User>(cUserString);
+            if (cUser.role_name.Trim().Equals("admin"))
+            {
+                orders = _OrderService.getAllWorkOrders();
+            }
+            else
+            {
+                orders = _OrderService.getAllWorkOrdersCreatedByUser(cUser.UserName);
+            }
+            return View(orders);
+        }
+
         public IActionResult create(string ApmID)
         {
             string cUserString = _contx.HttpContext.Session.GetString("cUser");
             User cUser = JsonConvert.DeserializeObject<User>(cUserString);
             Appointment appointment = _appointmentService.getAppointmentByID(ApmID);
             _OrderService.createWorkOrderByAPM(appointment, cUser.UserName);
-            return View();
+            return Redirect("/user/order/view");
         }
     }
 }
---
+++ SWP_CarService_Final/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SWP_CarService_Final.Areas.User.Models;
+using SWP_CarService_Final.Models;
+using SWP_CarService_Final.Services;
+
+namespace SWP_CarService_Final.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly AppointmentService _appointmentService;
+        private readonly OrderService _OrderService;
+        private readonly IHttpContextAccessor _contx;
+        public OrderController(AppointmentService appointmentService, OrderService orderService, IHttpContextAccessor contx)
+        {
+            _appointmentService = appointmentService;
+            _OrderService = orderService;
+            _contx = contx;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult view()
+        {
+            string cCustomerString = _contx.HttpContext.Session.GetString("cCus");
+            Customer cCustomer = JsonConvert.DeserializeObject<Customer>(cCustomerString);
+            List<WorkOrder> workOrders = _OrderService.getAllWorkOrdersOfOwner(cCustomer.user_name);
+            return View(workOrders);
+        }
+    }
+}
