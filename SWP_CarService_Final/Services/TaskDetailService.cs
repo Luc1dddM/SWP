b1e4940468bc0156e6c8647bf36f3011f3b1a0c3
StarBui3010 <saobcce170038@fpt.edu.vn>
2023-10-24 08:59:55 +0700

    Commit Frontend Create Appointment

--- SWP_CarService_Final/Services/TaskDetailService.cs
+++ SWP_CarService_Final/Services/TaskDetailService.cs
@@ -42,7 +42,7 @@ namespace SWP_CarService_Final.Services
             return result;
         }
 
-        public TaskDetail getTaskDetailByID(string wodID)
+       /* public TaskDetail getTaskDetailByID(string wodID)
         {
             TaskService taskService = new TaskService();
             TaskDetail taskDetail = null;
@@ -69,6 +69,6 @@ namespace SWP_CarService_Final.Services
                     }
                 }
             }
-        }
+        }*/
     }
 }
